6b1e941c1db8fd2da415530fb4690b974cab1265
Vashira <vashimgs@gmail.com>
2020-02-10 11:27:17 +0000

    Added scrape method to vehicles controller

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -7,6 +7,18 @@ class VehiclesController < ApplicationController
     @vehicles = Vehicle.all
   end
 
+  def scrape
+    url = 'https://www.cars.com/shopping/sedan/'
+    response = VehiclesSpider.process(url)
+    if response[:status] == :completed && response[:error].nil?
+      flash.now[:notice] = "Successfully scraped url"
+    else
+      flash.now[:alert] = response[:error]
+    end
+  rescue StandardError => e
+    flash.now[:alert] = "Error: #{e}"
+  end
+  
   # GET /vehicles/1
   # GET /vehicles/1.json
   def show
