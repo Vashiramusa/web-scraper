bf7d5bb1f8dd44f602c002d62c3b9261c80b499a
Vashira <vashimgs@gmail.com>
2020-02-10 11:11:48 +0000

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :vehicles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :vehicles do
+    match '/scrape', to: 'vehicles#scrape', via: :post, on: :collection
+  end
+
+  root to: 'vehicles#index'
 end
